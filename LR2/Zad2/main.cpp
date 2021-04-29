287ed75011e1e73486b5f1a115bec06e3292ac74
Tigelt <54934888+Tigelt@users.noreply.github.com>
2021-04-29 04:17:57 +0300

    Create main.cpp

---
+++ LR2/Zad2/main.cpp
@@ -0,0 +1,11 @@
+// Это предварительные объявления функций, которые находятся в файле io.cpp
+int readNumber();
+void writeAnswer(int x);
+ 
+int main()
+{
+    int x = readNumber();
+    int y = readNumber();
+    writeAnswer(x+y);
+    return 0;
+}
